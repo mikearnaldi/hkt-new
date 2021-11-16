0e4ff89f926132d7043fe7a9dce24feee2d72e5a
Michael Arnaldi <arnaldimichael@gmail.com>
2021-11-16 17:26:52 +0000

    attempt at encoding bounds

--- src/prelude/hkt.ts
+++ src/prelude/hkt.ts
@@ -5,25 +5,38 @@ export interface Typeclass<F extends HKT> {
 }
 
 export interface HKT {
+  readonly $K: unknown
+
+  readonly K?: unknown
   readonly R?: unknown
   readonly E?: unknown
   readonly A?: unknown
   readonly type?: unknown
 }
 
-export type Kind<F extends HKT, R, E, A> = F extends { readonly type: unknown }
+export type Kind<F extends HKT, K extends F["$K"], R, E, A> = F extends {
+  readonly type: unknown
+}
   ? (F & {
+      readonly K: K
       readonly R: R
       readonly E: E
       readonly A: A
     })["type"]
   : {
       readonly _F: F
+      readonly _K: () => K
       readonly _R: (_: R) => void
       readonly _E: () => E
       readonly _A: () => A
     }
 
 export interface ComposeF<F extends HKT, G extends HKT> extends HKT {
-  readonly type: Kind<F, this["R"], this["E"], Kind<G, this["R"], this["E"], this["A"]>>
+  readonly type: Kind<
+    F,
+    this["K"],
+    this["R"],
+    this["E"],
+    Kind<G, this["K"], this["R"], this["E"], this["A"]>
+  >
 }
--- src/prelude/typeclasses.ts
+++ src/prelude/typeclasses.ts
@@ -7,31 +7,33 @@ import { instance } from "./utils.js"
 export interface Functor<F extends P.HKT> extends P.Typeclass<F> {
   readonly map: <A, B>(
     f: (a: A) => B
-  ) => <R, E>(fa: P.Kind<F, R, E, A>) => P.Kind<F, R, E, B>
+  ) => <K extends F["$K"], R, E>(fa: P.Kind<F, K, R, E, A>) => P.Kind<F, K, R, E, B>
 }
 
 export interface Pointed<F extends P.HKT> extends Functor<F> {
-  readonly of: <A>(a: A) => P.Kind<F, unknown, never, A>
+  readonly of: <A>(a: A) => P.Kind<F, never, unknown, never, A>
 }
 
 export interface Apply<F extends P.HKT> extends Functor<F> {
-  readonly ap: <R, E1, A>(
-    fa: P.Kind<F, R, E1, A>
-  ) => <R1, E, B>(fab: P.Kind<F, R1, E, (a: A) => B>) => P.Kind<F, R & R1, E | E1, B>
+  readonly ap: <K extends F["$K"], R, E1, A>(
+    fa: P.Kind<F, K, R, E1, A>
+  ) => <K1 extends F["$K"], R1, E, B>(
+    fab: P.Kind<F, K1, R1, E, (a: A) => B>
+  ) => P.Kind<F, K, R & R1, E | E1, B>
 }
 
 export function getApply<F extends P.HKT>(F: Monad<F>): Apply<F> {
   return instance({
     map: F.map,
     ap:
-      <R1, E1, A>(fa: P.Kind<F, R1, E1, A>) =>
-      <R2, E, B>(fab: P.Kind<F, R2, E, (a: A) => B>) =>
+      <K1 extends F["$K"], R1, E1, A>(fa: P.Kind<F, K1, R1, E1, A>) =>
+      <K2 extends F["$K"], R2, E, B>(fab: P.Kind<F, K2, R2, E, (a: A) => B>) =>
         pipe(
-          fa,
-          F.chain((a) =>
+          fab,
+          F.chain((f) =>
             pipe(
-              fab,
-              F.map((f) => f(a))
+              fa,
+              F.map((a) => f(a))
             )
           )
         )
@@ -48,17 +50,21 @@ export function getApplicative<F extends P.HKT>(F: Monad<F>): Applicative<F> {
 }
 
 export interface Monad<F extends P.HKT> extends Pointed<F> {
-  readonly chain: <A, R1, E1, B>(
-    f: (a: A) => P.Kind<F, R1, E1, B>
-  ) => <R, E>(fa: P.Kind<F, R, E, A>) => P.Kind<F, R & R1, E | E1, B>
+  readonly chain: <A, K1 extends F["$K"], R1, E1, B>(
+    f: (a: A) => P.Kind<F, K1, R1, E1, B>
+  ) => <K extends F["$K"], R, E>(
+    fa: P.Kind<F, K, R, E, A>
+  ) => P.Kind<F, K1, R & R1, E | E1, B>
 }
 
 export interface Traversable<F extends P.HKT> extends P.Typeclass<F> {
   readonly traverse: <G extends P.HKT>(
     G: Applicative<G>
-  ) => <A, B, RG, EG>(
-    f: (a: A) => P.Kind<G, RG, EG, B>
-  ) => <RF, EF>(self: P.Kind<F, RF, EF, A>) => P.Kind<G, RG, EG, P.Kind<F, RF, EF, B>>
+  ) => <A, B, KG extends G["$K"], RG, EG>(
+    f: (a: A) => P.Kind<G, KG, RG, EG, B>
+  ) => <KF extends F["$K"], RF, EF>(
+    self: P.Kind<F, KF, RF, EF, A>
+  ) => P.Kind<G, KG, RG, EG, P.Kind<F, KF, RF, EF, B>>
 }
 
 export interface Semigroup<A> {
@@ -66,11 +72,11 @@ export interface Semigroup<A> {
 }
 
 export interface Eitherable<F extends P.HKT> extends P.Typeclass<F> {
-  readonly either: <R, E, A>(
-    fa: P.Kind<F, R, E, A>
-  ) => P.Kind<F, R, never, Either<E, A>>
+  readonly either: <K extends F["$K"], R, E, A>(
+    fa: P.Kind<F, K, R, E, A>
+  ) => P.Kind<F, K, R, never, Either<E, A>>
 }
 
 export interface Failable<F extends P.HKT> extends P.Typeclass<F> {
-  readonly fail: <E>(fa: E) => P.Kind<F, unknown, E, never>
+  readonly fail: <E>(fa: E) => P.Kind<F, never, unknown, E, never>
 }
